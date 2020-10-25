ec244596f789bb9bf18de0b977fb8cad03e64f60
CreationTribe <creationtribe@users.noreply.github.com>
2020-10-25 01:24:26 -0600

    Hrm, definitely not getting the graph I want - need to re-think lineage

--- TESTFILE.py
+++ TESTFILE.py
@@ -1,14 +1,23 @@
-spawn_seed = range(ord('a'), ord('c')+1)
+# spawn_seed = range(ord('a'), ord('c')+1)
+#
+# for _ in spawn_seed:
+# 	# NihiloSplit(mother_ID, spawn_seed, spawn_interactivity_map, SUBSTITUTE_VAR)
+# 	print(chr(_))
+#
+# print(ord('L'))
+#
+# t1 = None
+# print(t1)
+# if t1 is None:
+# 	print('0 == None')
+# else:
+# 	print('0 != none')
 
-for _ in spawn_seed:
-	# NihiloSplit(mother_ID, spawn_seed, spawn_interactivity_map, SUBSTITUTE_VAR)
-	print(chr(_))
 
-print(ord('L'))
+lineage = list()
+lineage.append('boob')
+lineage.append(['boob', 'goop', 'lunk'])
+lineage[1].append('snuggle')
 
-t1 = None
-print(t1)
-if t1 is None:
-	print('it is none')
-else:
-	print('is not none')
+print(type(lineage))
+print(lineage)
--- exnihilo.py
+++ exnihilo.py
@@ -1,55 +1,58 @@
+import numpy
+
 # Ex Nihilo
 #
 # 	    Maybe give it a split-base: 2, or 3, or 4, or 5? ... (how many interactive charges break from nothingness)
 # 	    allow to split and charge x-many generations out.
 # 	    then analyze generation interactivity?
-#
-#
-
-class NihiloSplit:
-	"""
-    PARAMETERS:
-        mother: mother's spawn id
-        self_ID: spawn_id + charge
-        spawn_seed: how many siblings are born together at each iteration (experiment later with oscillating spawn_seed)
-        charge ... perhaps the charge can be encoded into the ID, ie: pos/neg - up/dn - lft/rt - ... etc.
-            so an id could be neg-up-rt which also inherently represents its charge.
-            ** now or later - assign attributes to each charge with which interactions are initiated
-                ie: CC and CCW spin along X axis. Vibration (size pulse / directional pulse / angular pulse)
-                , direction (xyz), make-believe forces ... or is this going to create it's own forces?
-		generation ... generation can also be derived from ID, in that first gen will have
-			a single charge type, second gen will have two charge types.
-        sibling_IDs (list)
-	"""
-	def __init__(self, mother, iterations, spawn_seed, harmonic_disunion=None):
-		self.mother = mother
-		self.iteration = iterations - 1
-		self.harmonic_disunion = harmonic_disunion
-
-		if self.mother is None:
-			print("mama is None")
-			self.id = int(0)
-		elif type(self.mother) is int:
-			self.id = self.mother + 1
-
-		print(f'ID: {self.id} - mother: {self.mother} - iteration: {self.iteration} - charge: {self.harmonic_disunion}')
-
-		if self.iteration > 0:
-			_A = ord('a')
-			for disunion in range(_A, _A+spawn_seed):
-
-				NihiloSplit(self.mother, self.iteration, spawn_seed, chr(disunion))
 
 
+# === ReactionChamber ==================================================================================================
 class ReactionChamber:
 	"""
-	ReactionChamber
+	ReactionChamber()
 		Starts the NihiloSplit chain reaction
 		Analyzes possible interactivity among spawn
 	"""
+
 	def __init__(self, iterations, spawn_seed):
-		self.iterations = iterations
+		self.lineage = dict()
+		self.iterations = range(iterations)
 		self.spawn_seed = spawn_seed + 1 # adding 1 to account for the zero-slip in range()
+
+	# === NihiloSplit ==================================================================================================
+	class NihiloSplit:
+		"""
+		NihiloSplit()
+
+		PARAMETERS:
+			mother              : mother's self.id
+			id                  : iteration + harmonic_disunion (aka: "charge")
+			spawn_seed          : how many siblings are born together at each iteration (experiment later with oscillating spawn_seed)
+			harmonic_disunion   : (aka: "charge") the poly-vector interactive properties derived from spawn_seed.
+									If the spawn_seed is 2, then there are only two polar charges. If the spawn_seed is 3, then there are
+									3 interactive polar charges - one for each new spawn.
+
+				*** later, consider assigning attributes to each charge from which the interactive polar mechanics are derived.
+					ie i        : bipolar interactive system of CC and CCW spin along some axis.
+					ie ii       : Vibration (size pulse / directional pulse / angular pulse), etc.
+
+			iteration           : the count of each generation of "particle" splits
+			sibling_ids         : (maybe add this later - a list so each family from each generation is aware of its siblings
+		"""
+		def __init__(self, iteration, harmonic_disunion=None):
+			self.mother = iteration - 1
+			self.generation = iteration
+			self.harmonic_disunion = harmonic_disunion
+
+			if self.mother == -1:
+				self.id = int(0)
+			elif self.mother >= 0:
+				self.id = self.mother + 1
+
+			rc.lineage[f'{self.id}.{self.harmonic_disunion}'] = self
+
+	# === END: NihiloSplit =============================================================================================
 
 	# "sings" it all into existence - kicks off the NS chain reaction
 	def om(self):
@@ -66,9 +69,26 @@ class ReactionChamber:
 		# the separate charges according to the spawn_seed.
 
 		# let there be light mother fucker!
-		ns = NihiloSplit(None, self.iterations, self.spawn_seed)
+		rc.NihiloSplit(0, self.spawn_seed)
+
+		for iteration in self.iterations:
+			_A = ord('a')
+			for disunion in range(_A, _A + self.spawn_seed):
+				rc.NihiloSplit(iteration, chr(disunion))
+
+	def display_lineage(self):
+		for i in self.iterations:
+			_A = ord('a')
+			for disunion_harmonic in range(_A, _A + self.spawn_seed):
+				ns = self.lineage[f'{i}.{chr(disunion_harmonic)}']
+				print(f'{ns.mother}.{ns.generation}.{ns.harmonic_disunion}')
+
+		# for split in self.lineage:
+		# 	print(f'{split}')
+# === END: ReactionChamber =============================================================================================
 
 
 # ReactionChamber(iterations, spawn_seed)
-rc = ReactionChamber(3, 3)
+rc = ReactionChamber(5, 3)
 rc.om()
+rc.display_lineage()
